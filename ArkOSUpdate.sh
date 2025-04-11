79a334afb6720d371b450ba59000e910133c4289
AeolusUX <norucus@gmail.com>
2025-04-12 01:36:00 +0800

    update

---
+++ ArkOSUpdate.sh
@@ -0,0 +1,94 @@
+#!/bin/bash
+clear
+UPDATE_DATE="03292025"
+LOG_FILE="/home/ark/update$UPDATE_DATE.log"
+UPDATE_DONE="/home/ark/.config/.update$UPDATE_DATE"
+
+if [ -f "$UPDATE_DONE" ]; then
+	msgbox "No more updates available.  Check back later."
+	rm -- "$0"
+	exit 187
+fi
+
+if [ -f "$LOG_FILE" ]; then
+	sudo rm "$LOG_FILE"
+	sudo rm "$LOG_FILE"
+fi
+
+LOCATION="https://raw.githubusercontent.com/AeolusUX/ArkOS-R3XS-Updater/main"
+ISITCHINA="$(curl -s --connect-timeout 30 -m 60 http://demo.ip-api.com/json | grep -Po '"country":.*?[^\\]"')"
+
+if [ "$ISITCHINA" = "\"country\":\"China\"" ]; then
+  printf "\n\nSwitching to China server for updates.\n\n" | tee -a "$LOG_FILE"
+  LOCATION="https://raw.gitcode.com/norucus/ArkOS-R3XS-Updater/raw/main"
+fi
+
+sudo msgbox "MAKE SURE YOU SWITCHED TO MAIN SD FOR ROMS BEFORE YOU RUN THIS UPDATE. ONCE YOU PROCEED WITH THIS UPDATE SCRIPT, DO NOT STOP THIS SCRIPT UNTIL IT IS COMPLETED OR THIS DISTRIBUTION MAY BE LEFT IN A STATE OF UNUSABILITY.  Make sure you've created a backup of this sd card as a precaution in case something goes very wrong with this process.  You've been warned!  Type OK in the next screen to proceed."
+my_var=`osk "Enter OK here to proceed." | tail -n 1`
+
+#sudo msgbox "UPDATER IS CURRENTLY UNAVAILABLE. IT WILL BE BACK AGAIN, SOON."
+#my_var=`osk "TRY AGAIN LATER" | tail -n 1`
+
+echo "$my_var" | tee -a "$LOG_FILE"
+
+#if [ "$my_var" != "test" ] && [ "$my_var" != "TEST" ]; then
+if [ "$my_var" != "ok" ] && [ "$my_var" != "OK" ]; then
+
+  sudo msgbox "You didn't type OK.  This script will exit now and no changes have been made from this process."
+  printf "You didn't type OK.  This script will exit now and no changes have been made from this process." | tee -a "$LOG_FILE"	
+  exit 187
+fi
+
+c_brightness="$(cat /sys/class/backlight/backlight/brightness)"
+sudo chmod 666 /dev/tty1
+echo 255 > /sys/class/backlight/backlight/brightness
+touch $LOG_FILE
+tail -f $LOG_FILE >> /dev/tty1 &
+
+if [ ! -f "$UPDATE_DONE" ]; then
+
+	printf "\nUpdate retrorun.cfg to fix analog stick\n" | tee -a "$LOG_FILE"
+	sudo rm -rf /dev/shm/*
+	sudo wget -t 3 -T 60 --no-check-certificate "$LOCATION"/03302025/arkosupdate03302025.zip -O /dev/shm/arkosupdate03302025.zip -a "$LOG_FILE" || sudo rm -f /dev/shm/arkosupdate03302025.zip | tee -a "$LOG_FILE"
+	if [ -f "/dev/shm/arkosupdate03302025.zip" ]; then
+	  sudo unzip -X -o /dev/shm/arkosupdate03302025.zip -d / | tee -a "$LOG_FILE"
+	  sudo rm -fv /dev/shm/arkosupdate03302025.zip | tee -a "$LOG_FILE"
+	else
+	  printf "\nThe update couldn't complete because the package did not download correctly.\nPlease retry the update again." | tee -a "$LOG_FILE"
+	  sudo rm -fv /dev/shm/arkosupdate03302025.z* | tee -a "$LOG_FILE"
+	  sleep 3
+	  echo $c_brightness > /sys/class/backlight/backlight/brightness
+	  exit 1
+	fi
+	
+	printf "\nInstall inputs python3 module via pip3\n" | tee -a "$LOG_FILE"
+	sudo apt update -y | tee -a "$LOG_FILE"
+	sudo apt -y install python3-pip | tee -a "$LOG_FILE"
+	pip3 --retries 10 -v install inputs | tee -a "$LOG_FILE"
+	if [ $? != 0 ]; then
+	  printf "\nThe update couldn't complete because the inputs python package did not download correctly.\nPlease retry the update again." | tee -a "$LOG_FILE"
+	  sleep 3
+	  exit 1
+	fi
+	sudo systemctl daemon-reload
+
+	printf "\nUpdate retrorun.cfg to default swapped triggers to true for rk3326 devices and false for rk3566 devices\n" | tee -a "$LOG_FILE"
+	if [ ! -f "/boot/rk3566.dtb" ] && [ ! -f "/boot/rk3566-OC.dtb" ]; then
+	  sed -i "/retrorun_swap_l1r1_with_l2r2 \=/c\retrorun_swap_l1r1_with_l2r2 \= true" /home/ark/.config/retrorun.cfg
+	else
+	  sed -i "/retrorun_swap_l1r1_with_l2r2 \=/c\retrorun_swap_l1r1_with_l2r2 \= false" /home/ark/.config/retrorun.cfg
+	fi
+
+	printf "\nUpdate boot text to reflect current version of ArkOS\n" | tee -a "$LOG_FILE"
+	sudo sed -i "/title\=/c\title\=ArkOS 2.0 ($UPDATE_DATE)(AeUX)" /usr/share/plymouth/themes/text.plymouth
+	echo "$UPDATE_DATE" > /home/ark/.config/.VERSION
+
+	touch "$UPDATE_DONE"
+	rm -v -- "$0" | tee -a "$LOG_FILE"
+	printf "\033c" >> /dev/tty1
+	msgbox "Updates have been completed.  System will now restart after you hit the A button to continue.  If the system doesn't restart after pressing A, just restart the system manually."
+	echo $c_brightness > /sys/class/backlight/backlight/brightness
+	sudo reboot
+	exit 187
+
+fi
