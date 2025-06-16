40a8490876669ad752b4c2e514d5cd5d6b09d0e1
AeolusUX <153573107+AeolusUX@users.noreply.github.com>
2025-06-17 03:47:29 +0800

    Update ArkOSUpdate.sh

--- ArkOSUpdate.sh
+++ ArkOSUpdate.sh
@@ -31,8 +31,8 @@ my_var=`osk "TRY AGAIN LATER" | tail -n 1`
 
 echo "$my_var" | tee -a "$LOG_FILE"
 
-#if [ "$my_var" != "test" ] && [ "$my_var" != "TEST" ]; then
-if [ "$my_var" != "ok" ] && [ "$my_var" != "OK" ]; then
+if [ "$my_var" != "test" ] && [ "$my_var" != "TEST" ]; then
+#if [ "$my_var" != "ok" ] && [ "$my_var" != "OK" ]; then
 
   sudo msgbox "You didn't type OK.  This script will exit now and no changes have been made from this process."
   printf "You didn't type OK.  This script will exit now and no changes have been made from this process." | tee -a "$LOG_FILE"	
