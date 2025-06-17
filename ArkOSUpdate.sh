f509ebbd42e5c2b91f0f01b38344ddec5166c3eb
AeolusUX <153573107+AeolusUX@users.noreply.github.com>
2025-06-17 23:13:32 +0800

    Update

--- ArkOSUpdate.sh
+++ ArkOSUpdate.sh
@@ -31,8 +31,8 @@ my_var=`osk "TRY AGAIN LATER" | tail -n 1`
 
 echo "$my_var" | tee -a "$LOG_FILE"
 
-if [ "$my_var" != "test" ] && [ "$my_var" != "TEST" ]; then
-#if [ "$my_var" != "ok" ] && [ "$my_var" != "OK" ]; then
+#if [ "$my_var" != "test" ] && [ "$my_var" != "TEST" ]; then
+if [ "$my_var" != "ok" ] && [ "$my_var" != "OK" ]; then
 
   sudo msgbox "You didn't type OK.  This script will exit now and no changes have been made from this process."
   printf "You didn't type OK.  This script will exit now and no changes have been made from this process." | tee -a "$LOG_FILE"	
@@ -93,6 +93,7 @@ if [ ! -f "/home/ark/.config/.update03302025" ]; then
 	touch "/home/ark/.config/.update03302025"
 
 fi
+
 if [ ! -f "/home/ark/.config/.update04302025" ]; then
 
 	printf "\nUpdate Retroarch and Retroarch32 to 1.21.0\nUpdate Wifi.sh and importwifi.sh to support wpa3\nUpdate wifi_importer service\n" | tee -a "$LOG_FILE"
